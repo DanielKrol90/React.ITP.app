c174c044e2afb5211b177e81c55b103c3afd1159
Daniel Król <danielkrol90@gmail.com>
2022-10-15 10:13:18 +0200

    adding index to folders

---
+++ src/constants/images.jsx
@@ -0,0 +1,26 @@
+import favicon from '../assets/favicon.png';
+import logo from '../assets/logo.png';
+import logo2 from '../assets/logo_2.png';
+import logo_new from '../assets/logo_new.png';
+import bg_mobile from '../assets/bg-mobile.jpg';
+
+import chicken from '../assets/heroHeader/chicken.jpg';
+import chicks from '../assets/heroHeader/chicks.jpg';
+import dogocat from '../assets/heroHeader/dogocat.jpg';
+import pigeon from '../assets/heroHeader/pigeon.jpg';
+import rabit from '../assets/heroHeader/rabit.jpg';
+import cows from '../assets/heroHeader/cows.jpg';
+
+export default {
+    favicon,
+    logo,
+    bg_mobile,
+    logo2,
+    logo_new,
+    chicken,
+    chicks,
+    dogocat,
+    pigeon,
+    rabit,
+    cows,
+}
